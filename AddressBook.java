701835bc847924fc8dfeef3a84726a491605efc1
unknown <narmada.vaddireddy97@gmail.com>
2023-03-04 22:20:22 +0530

    Added: Code to print new contact contacts from console

--- AddressBook.java
+++ AddressBook.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class AddressBook {
-    Contact details = new Contact("N", "V", "Hyd", "Hyd",
-            "telangana", "508243", "9346787765", "nv@gmail.com");
+    public void addContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of details to be added");
+        int noOfDetails = sc.nextInt();
+        Contact[] contactArr = new Contact[noOfDetails];
+        for (int i = 0; i < noOfDetails; i++) {
+            Contact contact = new Contact();
+            System.out.println("Enter First Name");
+            contact.setFIRST_NAME(sc.next());
+            System.out.println("Enter last Name");
+            contact.setLAST_NAME(sc.next());
+            System.out.println("Enter Address");
+            contact.setADDRESS(sc.next());
+            System.out.println("Enter Your City Name");
+            contact.setCITY(sc.next());
+            System.out.println("Enter Your State");
+            contact.setSTATE(sc.next());
+            System.out.println("Enter Zip");
+            contact.setZip(sc.next());
+            System.out.println("Enter Phone Number");
+            contact.setPHONE_NUMBER(sc.next());
+            System.out.println("Enter Email");
+            contact.setEMAIL_ID(sc.next());
+            contactArr[i] = contact;
+        }
+        for (int i = 0; i < noOfDetails; i++) {
+            System.out.println(contactArr[i]);
+        }
+    }
 }
---
+++ AddressBookMain.java
@@ -0,0 +1,6 @@
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addDetails = new AddressBook();
+        addDetails.addContact();
+    }
+}
