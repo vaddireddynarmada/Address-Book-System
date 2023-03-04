4c6d5d78b07cbe56dfeb4effdc1cb4c79c500c3b
unknown <narmada.vaddireddy97@gmail.com>
2023-03-04 22:50:17 +0530

    Added: Code to print multiple person details in addressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,13 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    static Scanner sc = new Scanner(System.in);
-    static int noOfDetails = sc.nextInt();
-    static Contact[] contactArr = new Contact[noOfDetails];
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    ArrayList<Contact> addDetails = new ArrayList<Contact>(n);
 
     public void addContact() {
-        System.out.println("Enter number of details to be added");
-        for (int i = 0; i < noOfDetails; i++) {
+        System.out.println("number of details you want to enter");
+        for (int i = 1; i <= n; i++) {
             Contact contact = new Contact();
             System.out.println("Enter First Name");
             contact.setFIRST_NAME(sc.next());
@@ -25,73 +26,67 @@ public class AddressBook {
             contact.setPHONE_NUMBER(sc.next());
             System.out.println("Enter Email");
             contact.setEMAIL_ID(sc.next());
-            contactArr[i] = contact;
+            addDetails.add(contact);
         }
-        for (int i = 0; i < noOfDetails; i++) {
-            System.out.println(contactArr[i]);
-        }
+        System.out.println(addDetails);
     }
 
     public void editContact() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter name to Edit :  ");
         String name = sc.nextLine();
-        for (int i = 0; i < contactArr.length; i++) {
-            if (name.equals(contactArr[i].getFIRST_NAME())) {
+        for (Contact contact : addDetails) {
+            if (contact.getFIRST_NAME().equals(name)) {
                 System.out.println("enter which details wants to edit in contact");
                 String editDetails = sc.next().toUpperCase();
                 switch (editDetails) {
                     case "FIRSTNAME":
                         System.out.println("enter firstname");
-                        contactArr[i].setFIRST_NAME(sc.next());
+                        contact.setFIRST_NAME(sc.next());
                         break;
                     case "LASTNAME":
                         System.out.println("enter lastname");
-                        contactArr[i].setLAST_NAME(sc.next());
+                        contact.setLAST_NAME(sc.next());
                         break;
                     case "ADDRESS":
                         System.out.println("enter firstname");
-                        contactArr[i].setADDRESS(sc.next());
+                        contact.setADDRESS(sc.next());
                         break;
                     case "CITY":
                         System.out.println("enter city");
-                        contactArr[i].setCITY(sc.next());
+                        contact.setCITY(sc.next());
                         break;
                     case "STATE":
                         System.out.println("enter state");
-                        contactArr[i].setSTATE(sc.next());
+                        contact.setSTATE(sc.next());
                         break;
-                    case "zip":
+                    case "ZIP":
                         System.out.println("enter zip");
-                        contactArr[i].setZip(sc.next());
+                        contact.setZip(sc.next());
                         break;
                     case "PHONENUMBER":
                         System.out.println("enter phoneNumber");
-                        contactArr[i].setPHONE_NUMBER(sc.next());
+                        contact.setPHONE_NUMBER(sc.next());
                         break;
                     case "EMAILID":
                         System.out.println("enter email-id");
-                        contactArr[i].setEMAIL_ID(sc.next());
+                        contact.setEMAIL_ID(sc.next());
                         break;
-                    default:
-                        System.out.println("not valid in details");
                 }
-                System.out.println(contactArr[i]);
+                System.out.println(contact);
             }
         }
     }
 
     public void deleteContact() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name to Edit :  ");
+        System.out.println("Enter name to delete :  ");
         String name = sc.nextLine();
-        String delete;
-        for (int i = 0; i < contactArr.length; i++) {
-            if (name.equals(contactArr[i].getFIRST_NAME())) {
-                delete = null;
-            } else {
-                System.out.println("name is not present");
+        for (Contact contact : addDetails) {
+            if (contact.getFIRST_NAME().equals(name)) {
+                addDetails.remove(name);
             }
         }
     }
+
 }
