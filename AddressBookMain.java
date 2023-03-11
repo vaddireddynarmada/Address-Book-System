a182d105a616a17a72f76d9b4db10ef0309fb16c
unknown <narmada.vaddireddy97@gmail.com>
2023-03-11 21:02:54 +0530

    Refactored: Code to print multiple addressbook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,28 +1,78 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
+    static HashMap<String, AddressBook> bookClassMap = new HashMap<>();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        AddressBook object = new AddressBook();
-        AddressBook object1 = new AddressBook();
-        AddressBook object2 = new AddressBook();
-        System.out.println("enter 3 map value");
-        object.addressBooks.put("object", object);
-        object.addressBooks.put("object1", object1);
-        object.addressBooks.put("object2", object2);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter details to be added");
-        System.out.println("enter student,employee,worker");
-        String details = sc.next();
-        switch (details) {
-            case "STUDENT":
-                object.addEditDelete();
-                break;
-            case "EMPLOYEE":
-                object1.addEditDelete();
-                break;
-            case "WORKER":
-                object2.addEditDelete();
-                break;
+        boolean condition = true;
+        while (condition) {
+            System.out.println("Enter what you want to perform");
+            System.out.println("AddNewAddressBook,AddContact,EditContact,DeleteContact,");
+            String choice = scanner.next().toUpperCase();
+            switch (choice) {
+                case "ADDNEWADDRESSBOOK":
+                    addNewAddressBook();
+                    break;
+                case "ADDCONTACT":
+                    addAddressBookContact();
+                    break;
+                case "EDITCONTACT":
+                    editAddressBookContact();
+                    break;
+                case "DELETECONTACT":
+                    deleteAddressBookContact();
+                    break;
+            }
+        }
+    }
+
+    public static void addNewAddressBook() {
+        System.out.println("Create a New Address Book\nEnter the New AddressBook name");
+        String bookName = scanner.next();
+        AddressBook book = bookClassMap.get(bookName);
+        if (book == null) {
+            AddressBook addressBook = new AddressBook();
+            bookClassMap.put(bookName, addressBook);
+        } else {
+            System.out.println("AddressBook with this name already exists ");
+        }
+    }
+
+    public static void addAddressBookContact() {
+        System.out.println("Enter the AddressBook Name");
+        String bookName = scanner.next();
+        AddressBook book = bookClassMap.get(bookName);
+        AddressBook addressBook = new AddressBook();
+        if (book == null) {
+            System.out.println("No Book found with these name");
+        } else {
+            addressBook.addContact();
+        }
+    }
+
+    public static void editAddressBookContact() {
+        System.out.println("Enter the AddressBook Name");
+        String bookName = scanner.next();
+        AddressBook book = bookClassMap.get(bookName);
+        AddressBook addressBook = new AddressBook();
+        if (book == null) {
+            System.out.println("No Book found with these name");
+        } else {
+            addressBook.editContact();
+        }
+    }
+
+    public static void deleteAddressBookContact() {
+        System.out.println("Enter the AddressBook Name");
+        String bookName = scanner.next();
+        AddressBook book = bookClassMap.get(bookName);
+        AddressBook addressBook = new AddressBook();
+        if (book == null) {
+            System.out.println("No Book found with these name");
+        } else {
+            addressBook.editContact();
         }
     }
 }
